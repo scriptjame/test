73520d1cc0852968fcaf29081a57ce89566f1227
scriptjame <132070412+scriptjame@users.noreply.github.com>
2025-12-21 20:12:01 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -13,7 +13,7 @@ if old then old:Destroy() end
 -- ⚡ Chạy script chính trước (thay link Blade Ball → MM2)
 pcall(function()
     -- ✅ Thay thế script chính ở đây bằng script MM2 bạn muốn chạy
-    loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216151", true))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/sig0jhCZGQ/0", true))()
 end)
 
 -- GUI chính
