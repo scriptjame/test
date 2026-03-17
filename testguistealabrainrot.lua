39af74c6e9fc3f671ef0effb64591b317bf8f574
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-03-18 02:11:04 +0700

    Update testguistealabrainrot.lua

--- testguistealabrainrot.lua
+++ testguistealabrainrot.lua
@@ -150,14 +150,14 @@ scroll.CanvasSize=UDim2.new(0,0,0,list.AbsoluteContentSize.Y+10)
 end
 
 -- scripts
-createBtn("Makzinn Hub","https://raw.githubusercontent.com/MagoKazinn/Makzinn_hub/main/makzinn_Hub")
-createBtn("Argon Hub X","https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua")
-createBtn("Frostware Hub need key","https://raw.githubusercontent.com/Fsploit/F-R-O-S-T-W-A-R-E/refs/heads/main/Main")
-createBtn("Catsus Hub","https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua")
-createBtn("RX Hub","https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.Loader.lua")
+createBtn("Hun Hub","https://pastebin.com/raw/g2W4zneW")
+createBtn("Rey Hub","https://raw.githubusercontent.com/javi-esp/Rey/main/sab.lua")
+createBtn("Stear Hub","https://pastefy.app/3P0o3D66/raw")
+createBtn("Guizzy Hub","https://raw.githubusercontent.com/GuizzyisbackV2LOL/Inkxsteal/refs/heads/main/.lua")
+createBtn("Monkey Hub","https://raw.githubusercontent.com/dontasklol/Stealingurbrainrots/refs/heads/main/Kurd%20hub")
 
-createBtn("Allusive",nil,true,true)
-createBtn("UwU",nil,true,true)
+createBtn("Star Hub",nil,true,true)
+createBtn("Sun Hub",nil,true,true)
 
 -- socials
 local socialFrame=Instance.new("Frame",frame)
